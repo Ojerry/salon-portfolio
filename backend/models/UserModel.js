84b7882fa0e126fae70bcb41622eb65670b3a3c3
Ojerry <onotu128@gmail.com>
2023-01-05 14:43:48 +0100

    add usermodel

---
+++ backend/models/UserModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+// Schema
+const userSchema = new mongoose.Schema(
+    {
+        fullName:  { type: String, required: [true, 'Full Name Is Required'] },
+        email: { type: String, required: [true, 'Email is Required'],},
+        password:  { type: String, select: false,},
+        phone:  { type: Number, default: null },
+        business_type:  { type: String, default: '' },
+        business_name:  { type: String, default: '' },
+        location:  { type: String, default: '' },
+        city:  { type: String, default: '' },
+        profilePicture: String,
+        productAndServices: {
+            type: Array, default:[{photo: {}, rating:''}]
+        },
+    },
+    {
+      createdAt: { type: Date, default: Date.now() },
+      updatedAt: { type: Date, default: Date.now() },
+    },
+)
+
+const User = mongoose.model('User', userSchema)
+
+// exports.User = User
+module.exports = User
